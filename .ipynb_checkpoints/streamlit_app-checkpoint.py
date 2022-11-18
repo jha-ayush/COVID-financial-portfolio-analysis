5f333228f667a7370a2f688932226a5083eb435f
Ayush <jha-ayush@users.noreply.github.com>
2022-11-18 13:59:24 -0800

    streamlit multipages

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -23,13 +23,6 @@ local_css("style/style.css")
     
 # Load assets 
 lottiefiles_gif=load_lottieurl("https://assets7.lottiefiles.com/private_files/lf30_ghysqmiq.json")
-img_amt_pan_tearsheet=Image.open("./Images/amt_pan_tearsheet.png")
-img_amzn_pan_tearsheet=Image.open("./Images/amzn_pan_tearsheet.png")
-img_iyr_pan_tearsheet=Image.open("./Images/iyr_pan_tearsheet.png")
-img_rth_pan_tearsheet=Image.open("./Images/rth_pan_tearsheet.png")
-img_xle_pan_tearsheet=Image.open("./Images/xle_pan_tearsheet.png")
-img_xom_pan_tearsheet=Image.open("./Images/xom_pan_tearsheet.png")
-img_spy_pan_tearsheet=Image.open("./Images/spy_pan_tearsheet.png")
 
 """
 ## Welcome to the COVID financial portfolio analyzer
@@ -46,34 +39,7 @@ with st.container():
      with left_column:
           st.title("Financial analysis of stocks & ETFs against the S&P500 index")
      with right_column:
-        st_lottie(lottiefiles_gif,height="75",key="finance") 
-        
-# Tearsheets
-with st.container():
-    st.write("---")
-    st.header("Tearsheets - AMT, AMZN, IYR, RTH, XLE, XOM, SPY")
-    image_column_1,image_column_2,image_column_3,image_column_4,image_column_5,image_column_6,image_column_7=st.columns((1,1,1,1,1,1,1))
-    with image_column_1:
-        st.write("American Tower Corp")
-        st.image(img_amt_pan_tearsheet)
-    with image_column_2:
-        st.write("Amazon.com, Inc.")
-        st.image(img_amzn_pan_tearsheet)
-    with image_column_3:
-        st.write("iShares US Real Estate ETF")
-        st.image(img_iyr_pan_tearsheet)
-    with image_column_4:
-        st.write("VanEck Retail ETF")
-        st.image(img_rth_pan_tearsheet)
-    with image_column_5:
-        st.write("Energy Select Sector SPDR ETF Fund")
-        st.image(img_xle_pan_tearsheet)
-    with image_column_6:
-        st.write("Exxon Mobil Corp")
-        st.image(img_xom_pan_tearsheet)
-    with image_column_7:
-        st.write("S&P500 ETF")
-        st.image(img_spy_pan_tearsheet)
+        st_lottie(lottiefiles_gif,height="75",key="finance")
         
 # Contact Form
 with st.container():
@@ -83,7 +49,7 @@ with st.container():
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
     contact_form = """
-    <form action="https://formsubmit.co/jha.ayush85@gmail.com" method="POST">
+    <form action="https://formsubmit.co/firobeid92@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
