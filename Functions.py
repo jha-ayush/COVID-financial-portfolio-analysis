27efa272c1d5bc4b171a26b1e6f2202a2d5a07c0
Kranthi Mitta <kranthi.mitta@sunrun.global>
2022-11-19 16:07:48 -0800

    Functions Code Push

--- Functions.py
+++ Functions.py
@@ -18,14 +18,17 @@ from pytz import timezone
 import pyfolio as pf
 from IPython.display import display
 import matplotlib
-
+import re
+'https://api.polygon.io/v2/aggs/ticker/AMZN/range/1/day/2021-03-01/2022-03-01?adjusted=true&apiKey=hpeq4ZPUPErMkP2WZv5pNhAtO002i4ZF'
 def get_prices(start_date,end_date,universe):
     data = pd.DataFrame()
     # end_at = datetime.datetime.now() 
     # begin_from = end_at + datetime.timedelta(days=-5000)
     for t in tqdm(universe):
-        print(t)
-        print(len(data))
+        #print(t)
+        #print(start_date)
+        #print(end_date)
+        #print(len(data))
         # for start in pd.date_range(start = begin_from, end = end_at,normalize=True,freq = '88D'):
         #     end = start + datetime.timedelta(days = 88)
         #     end = str(int((end - datetime.datetime(1970,1,1)).total_seconds()))
@@ -35,8 +38,9 @@ def get_prices(start_date,end_date,universe):
         except json.JSONDecodeError:
             pass
         try:
-            # print(r.json())
+            
             temp_data = pd.DataFrame(r.json()['results'])
+            #print(temp_data)
             temp_data['ticker'] = t
             data = data.append(temp_data, ignore_index=True)
             print(t+':'+str(pd.to_datetime(start_date, unit='s'))+':'+str(pd.to_datetime(end_date, unit='s')))
@@ -55,6 +59,7 @@ def get_prices(start_date,end_date,universe):
             continue
         sleep(0.25)
 #     data.to_csv(file_name,index = False)
+    print(data)
     data.t = pd.to_datetime(data.t, unit = 'ms')
     columns_name = {'t': 'time', 'o': 'Open', 'c': 'Close', 'h': 'High', 'l': 'Low', 'v': 'Volume'} 
     data= data.rename(columns = columns_name)
@@ -64,10 +69,9 @@ def get_prices(start_date,end_date,universe):
     data = data.dropna()
     #data.to_csv(file_name, index = False)
     return data
+    
 
-def date_ranges():
-    start_date = "2017-03-01"
-    end_date = "2020-03-01"
+def date_ranges(start_date,end_date,ticker_list):
     all_data = get_prices(start_date=start_date, end_date=end_date, universe=ticker_list)    
     return all_data
 
@@ -75,40 +79,13 @@ def get_ticker_data_df(all_data, ticker_symbol):
     idx = pd.IndexSlice
     daily_returns_df = all_data.loc[idx[:,ticker_symbol],['daily_returns']]
     return daily_returns_df
-"""
-def amzn_data(all_data):
-    AMZN_daily_returns_df = all_data.loc[idx[:,'AMZN'],['daily_returns']]
-    return AMZN_daily_returns_df
 
-def rth_data(all_data):
-    RTH_daily_returns_df = all_data.loc[idx[:,'RTH'],['daily_returns']]
-    return RTH_daily_returns_df
-
-def amt_data(all_data):
-    AMT_daily_returns_df = all_data.loc[idx[:,'AMT'],['daily_returns']]
-    return AMT_daily_returns_df
-
-def iyr_data(all_data):
-    IYR_daily_returns_df = all_data.loc[idx[:,'IYR'],['daily_returns']]
-    return IYR_daily_returns_df
-
-def xom_data(all_data):            
-    XOM_daily_returns_df = all_data.loc[idx[:,'XOM'],['daily_returns']]
-    return XOM_daily_returns_df
-
-def xle_data(all_data):    
-    XLE_daily_returns_df = all_data.loc[idx[:,'XLE'],['daily_returns']]
-    return XLE_daily_returns_df
-
-def spy_data(all_data):    
-    SPY_daily_returns_df = all_data.loc[idx[:,'SPY'],['daily_returns']]
-    return SPY_daily_returns_df
-"""
-def create_prepandemic_df(ticker_df_list,ticker_list):
+def create_concat_df(ticker_df_list,ticker_list):
     concat_df = pd.concat(ticker_df_list,axis=1, keys=ticker_list)
     return concat_df    
 
-def create_prepandemic_mean_df(concat_df,ticker_type_list):
+#Mean Function
+def create_mean_df(concat_df,ticker_type_list):
     mean_df=pd.DataFrame(concat_df.mean())
     mean_df=mean_df.reset_index()
     del mean_df['level_1']
@@ -116,17 +93,35 @@ def create_prepandemic_mean_df(concat_df,ticker_type_list):
     mean_df['Ticker_type']=ticker_type_list
     return mean_df
 
+
+#Standard Deviation Function
+def create_std_df(concat_df,ticker_type_list):
+    std_df=pd.DataFrame(concat_df.std())
+    std_df=std_df.reset_index()
+    del std_df['level_1']
+    std_df.columns=['Ticker','Mean']
+    std_df['Ticker_type']=ticker_type_list
+    return std_df
+
+#Variance Function
+def get_var(concat_df,column):
+    variance=concat_df[column].var()
+    return vaariance 
+
+#Database Connection
 def create_connection():
     database_connection_string = 'sqlite:///'
     engine = sqlalchemy.create_engine(database_connection_string)
     #print(engine.table_names())
     return engine
 
+#SQL Table Creation
 def create_sql_table(mean_df):
     engine = create_connection()
     mean_df.to_sql('portfolio_mean',engine)
     return engine
     
+ #Top Stock Function   
 def top_stock(engine):
     sel_max_mean="""select Ticker from portfolio_mean where mean= (select max(Mean) from portfolio_mean where Ticker_type='Stock')"""
     max_mean=list(engine.execute(sel_max_mean))
@@ -135,7 +130,7 @@ def top_stock(engine):
     else:
         print("Check code")
 
-
+#Bottom Stock Function
 def bottom_stock(engine):
     sel_min_mean="""select Ticker from portfolio_mean where mean= (select min(mean) from portfolio_mean where Ticker_type='Stock')"""
     min_mean=list(engine.execute(sel_min_mean))
@@ -144,6 +139,8 @@ def bottom_stock(engine):
     else:
         print("Check code")
 
+        
+#Top ETF Function        
 def top_etf(engine):
     sel_port_max_mean="""select Ticker from portfolio_mean where mean= (select max(Mean) from portfolio_mean where Ticker_type='ETF')"""
     max_mean=list(engine.execute(sel_port_max_mean))
@@ -152,6 +149,8 @@ def top_etf(engine):
     else:
         print("Check code")
 
+        
+#Bottom ETF Function        
 def bottom_etf(engine):
     sel_port_min_mean="""select Ticker from portfolio_mean where mean=(select min(mean) from portfolio_mean where Ticker_type='ETF')"""
     min_mean=list(engine.execute(sel_port_min_mean))
@@ -160,55 +159,159 @@ def bottom_etf(engine):
     else:
         print("Check code")
 
+#Surpass SPY Function        
 def sur_spy(engine):
-    sel_port_sur_spy="""select Ticker from portfolio_mean where Mean>0.020825"""
+    sel_port_sur_spy="""select Ticker from portfolio_mean where Mean>0.000286"""
     sur_spy=list(engine.execute(sel_port_sur_spy))
-    return str(sur_spy)
-    
-    
+    #[('AMZN',), ('RTH',), ('AMT',)] performed better than SPY needs to be converted to AMZN,RTH,AMT performed better...
+    # new_sur_spy = map(get_ticker_string , sur_spy)
+    return pd.DataFrame(sur_spy, columns = ["Best Performers"])
+
+
+def get_ticker_string(str):
+    match = re.search(r'\(\'([A-Z]+)\',\)', str)
+    if match:
+        return match.group(1)
+    else:
+        print("RE pattern match failed. Found str : {str}")
+
+        
+#Variance Function        
+def get_variance_per_ticker(ticker_df_list,ticker_list):
+    var_dict = {} 
+    for idx in range(len(ticker_list)):
+        var_dict[ticker_list[idx]]=ticker_df_list[idx]['daily_returns'].var()
+    return pd.DataFrame(var_dict, index=[0])
+
+#Covariance Function
+def get_covariance_per_ticker(ticker_df_list,ticker_list):
+    covar_dict = {}
+    # Find index of SPY in ticker_list
+    try:
+        spy_idx=ticker_list.index("SPY")
+    except:
+        print("Cannot find SPY in ticker list. Cannot calculate Covariance based on SPY")
+        return
+        
+    # get dataframe for SPY based on index
+    SPY_df = ticker_df_list[spy_idx]
+    for idx in range(len(ticker_list)):
+        covar_dict[ticker_list[idx]]=ticker_df_list[idx]['daily_returns'].cov(SPY_df['daily_returns'])
+    return pd.DataFrame(covar_dict, index=[0])
+                                                    
+#Beta Function                                                    
+def get_beta_per_ticker(covar_df,ticker_df_list,ticker_list):
+    beta_dict ={}
+    try:
+        spy_idx=ticker_list.index("SPY")
+    except:
+        print("Cannot find SPY in ticker list. Cannot calculate Covariance based on SPY")
+        return
+        
+    # get dataframe for SPY based on index
+    SPY_df = ticker_df_list[spy_idx]
+    SPY_var = float(SPY_df['daily_returns'].var())
+    for idx in range(len(ticker_list)):
+        tk = ticker_list[idx]
+        beta_dict[tk] = float(covar_df.iloc[0,idx])/SPY_var
+    return pd.DataFrame(beta_dict, index=[0])                                        
+                                                    
+                                                    
+                                                    
 if __name__ =="__main__":
-    
-    user_choice = questionary.select("What would you like answers to\
-                                 Choose 1 for: Which stock performed well in pre pandemic\
-                                 Choose 2 for: Which etf performed well in pre pandemic\
-                                 Choose 3 for: Which ticker performed better than SPY\
-                                 Choose 4 for: Which stock performed inversely in pre pandemic\
-                                 Choose 5 for: Which ETF performed inversely in pre pandemic?"
+#CLI OPTIONS    
+    user_choice_period = questionary.select("select a period?",choices = ["pre-pandemic","pandemic","post-pandemic"]).ask()
+    start_date=""
+    end_date=""
+    if user_choice_period == "pre-pandemic":
+        start_date = "2017-03-01"
+        end_date = "2020-02-29"
+    elif user_choice_period == "pandemic":       
+        start_date = "2020-03-01"
+        end_date = "2021-02-28"
+    elif user_choice_period == "post-pandemic":
+        start_date = "2021-03-01"
+        end_date = "2022-03-01"
+    else:
+        print("period not valid")
+    answers = "What would you like answers to \n\
+                                 Choose 1 for: Which stock performed well \n\
+                                 Choose 2 for: Which etf performed well \n\
+                                 Choose 3 for: Which ticker performed better than SPY \n\
+                                 Choose 4 for: Which stock performed inversely \n\
+                                 Choose 5 for: Which ETF performed inversely?"
+    user_choice_question = questionary.select(answers
     ,choices = ["1","2","3","4","5"]).ask()
 
-
-    
+#Ticker List    
     ticker_list = ["AMZN", "RTH", "AMT", "IYR", "XOM", "XLE", "SPY"]
     ticker_type_list=['Stock','ETF','Stock','ETF','Stock','ETF','Index']
     POLYGON_API_KEY = 'JQfBpF3NpcYjuBdMiXeUr6q54XafY_pQ'
-    all_data_df = date_ranges()
+    
+#All Ticker Data    
+    all_data_df = date_ranges(start_date,end_date,ticker_list)
+    #display(all_data_df)
+    
+#List of Data Frames for Individual Tickers     
     ticker_df_list = []
     for tk in ticker_list:
         ticker_df_list.append(get_ticker_data_df(all_data_df,tk))
+        
     if len(ticker_df_list) != len(ticker_list):
         print("ticker df list length not matching ticker list")
-             
-    prepandemic_df = create_prepandemic_df(ticker_df_list,ticker_list)
-    prepandemic_mean_df = create_prepandemic_mean_df(prepandemic_df, ticker_type_list)
-    display(prepandemic_mean_df)
-    mysqlengine= create_sql_table(prepandemic_mean_df)
-    if user_choice =="1":
-        mytopstock = top_stock(mysqlengine)
-        print(mytopstock)
-    elif user_choice =="2":
-        mytopetf = top_etf(mysqlengine)
-        print(mytopetf)
-    elif user_choice =="3":
+        
+        
+#Concatenated Data             
+    my_concat_df = create_concat_df(ticker_df_list,ticker_list)
+    #display(my_concat_df)
+    my_mean_df = create_mean_df(my_concat_df,ticker_type_list)
+    #display(my_mean_df)
+    mysqlengine= create_sql_table(my_mean_df)
+    
+    
+ #CLI OPTIONS   
+    if user_choice_question =="1":
+        mytopstock = get_ticker_string(top_stock(mysqlengine))
+        print(f'{mytopstock} is top performing stock')
+    elif user_choice_question =="2":
+        mytopetf = get_ticker_string(top_etf(mysqlengine))
+        print(f'{mytopetf} is top performing ETF')
+    elif user_choice_question =="3":
         surspy=sur_spy(mysqlengine)
-        print(surspy)
-    elif user_choice =="4":
-        mybottomstock = bottom_stock(mysqlengine)
-        print(mybottomstock)
-    elif user_choice =="5":
-        mybottometf = bottom_etf(mysqlengine)
-        print(mybottometf)
+        print(f'{str(surspy)} \nPerformed better than SPY')
+    elif user_choice_question =="4":
+        mybottomstock = get_ticker_string(bottom_stock(mysqlengine))
+        print(f'{mybottomstock} is worst performing Stock')
+    elif user_choice_question =="5":
+        mybottometf = get_ticker_string(bottom_etf(mysqlengine))
+        print(f'{mybottometf} is worst performing ETF')
     else:
-        print("User choice is not valid")
+        print('User choice is not valid')
+        
+        
+#Variance
+    var_df = get_variance_per_ticker(ticker_df_list,ticker_list)
+    display(var_df)
+    
+    
+#Covariance    
+    covar_df = get_covariance_per_ticker(ticker_df_list,ticker_list)
+    display(covar_df)
+    
+    
+#Beta    
+    beta_df = get_beta_per_ticker(covar_df,ticker_df_list,ticker_list)
+    display(beta_df)
+    
+    
+#Misc    
+    #variance = create_var(my_df,daily_returns)
+    #display(variance)                     
+    #mean_df = create_mean_df(my_df, ticker_type_list)
+    #std_df = create_std_df(my_df, ticker_type_list)
+    #display(std_df)
+    #display(mean_df)
+    
     
     
     
