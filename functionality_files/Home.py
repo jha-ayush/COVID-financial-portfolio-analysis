5a5ec6624f830e0904a4f0687045630b60fc23a8
Ayush <jha-ayush@users.noreply.github.com>
2022-12-22 20:50:09 -0800

    exec summary update

--- functionality_files/Home.py
+++ functionality_files/Home.py
@@ -52,6 +52,8 @@ def local_css(file_name):
     """
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True) 
+# load css file
+local_css("./style/style.css")          
         
 # functions
 def get_prices(start_date,end_date,universe):
