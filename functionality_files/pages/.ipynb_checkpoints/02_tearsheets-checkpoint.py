fbdfeb098d79ffd1e617713e91eff9500d0ac706
Ayush <jha-ayush@users.noreply.github.com>
2022-12-23 15:23:25 -0800

    updated functionality files

--- functionality_files/pages/.ipynb_checkpoints/02_tearsheets-checkpoint.py
+++ functionality_files/pages/.ipynb_checkpoints/02_tearsheets-checkpoint.py
@@ -7,7 +7,7 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 
 # Set page configurations - ALWAYS at the top
-st.set_page_config(page_title="COVID portfolio analyzer",page_icon=":bar_chart:",layout="wide")
+st.set_page_config(page_title="COVID portfolio analyzer",page_icon=":bar_chart:",layout="centered")
 
 
 # Use local style.css file
@@ -15,11 +15,8 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
         
-local_css("./style/style.css")  
+local_css("./style/style.css")        
 
-ticker_list = ["AMZN", "RTH", "AMT", "IYR", "XOM", "XLE", "SPY"]
-POLYGON_API_KEY = 'JQfBpF3NpcYjuBdMiXeUr6q54XafY_pQ'
-    
 # Load assets 
 img_amt_pan_tearsheet=Image.open("../Images/AMT.png")
 img_amzn_pan_tearsheet=Image.open("../Images/AMZN.png")
@@ -28,43 +25,44 @@ img_rth_pan_tearsheet=Image.open("../Images/RTH.png")
 img_xle_pan_tearsheet=Image.open("../Images/XLE.png")
 img_xom_pan_tearsheet=Image.open("../Images/XOM.png")
 img_spy_pan_tearsheet=Image.open("../Images/SPY.png")
-        
-# Tearsheets
-with st.container():
-    st.header("Tearsheets - AMT, AMZN, IYR, RTH, XLE, XOM, SPY")
-    st.write("These are teaser metric snapshots of the tickers")
-    st.write("---")
-    
-    width = st.slider('What width would you like in pixels?', 0, 700, 350)
-    
-    # iterate through the items in the array
-    for ticker in ticker_list:
-        # load the image file for the ticker
-        image=Image.open(f"../Images/{ticker}.png")
-        # display the image
-        image.show()
-             
 
+# Create a dictionary for the tickers
+imgs_dict={"AMT":img_amt_pan_tearsheet,"AMZN":img_amzn_pan_tearsheet,"IYR":img_iyr_pan_tearsheet,"RTH":img_rth_pan_tearsheet,"XLE":img_xle_pan_tearsheet,"XOM":img_xom_pan_tearsheet,"SPY":img_spy_pan_tearsheet}
+
+# Add Title
+st.title(f"Tearsheets")
+# Create a selectbox
+img_dict=st.selectbox("Which ticker tearsheet would you like to view?",imgs_dict)
+
+# Display specific ticker for each symbol
+if img_dict == "AMT":
+    width = st.slider('What width would you like in pixels?', 0, 700, 350)
+    image = Image.open("../Images/AMT.png")
+    st.image(image, caption='AMT Tearsheet', width=width)
+elif img_dict == "AMZN":
+    width = st.slider('What width would you like in pixels?', 0, 700, 350)
+    image = Image.open("../Images/AMZN.png")
+    st.image(image, caption='AMZN Tearsheet', width=width)
+elif img_dict == "IYR":
+    width = st.slider('What width would you like in pixels?', 0, 700, 350)
+    image = Image.open("../Images/IYR.png")
+    st.image(image, caption='IYR Tearsheet', width=width)
+elif img_dict == "RTH":
+    width = st.slider('What width would you like in pixels?', 0, 700, 350)
+    image = Image.open("../Images/RTH.png")
+    st.image(image, caption='RTH Tearsheet', width=width)
+elif img_dict == "XLE":
+    width = st.slider('What width would you like in pixels?', 0, 700, 350)
+    image = Image.open("../Images/XLE.png")
+    st.image(image, caption='XLE Tearsheet', width=width)
+elif img_dict == "XOM":
+    width = st.slider('What width would you like in pixels?', 0, 700, 350)
+    image = Image.open("../Images/XOM.png")
+    st.image(image, caption='XOM Tearsheet', width=width)
+else:
+    width = st.slider('What width would you like in pixels?', 0, 700, 350)
+    image = Image.open("../Images/SPY.png")
+    st.image(image, caption='SPY Tearsheet', width=width)
     
-    # image_column_1,image_column_2,image_column_3,image_column_4,image_column_5,image_column_6,image_column_7=st.columns((1,1,1,1,1,1,1))
-    # with image_column_1:
-    #     st.write("American Tower Corp")
-    #     st.image(img_amt_pan_tearsheet)
-    # with image_column_2:
-    #     st.write("Amazon.com, Inc.")
-    #     st.image(img_amzn_pan_tearsheet)
-    # with image_column_3:
-    #     st.write("iShares US Real Estate ETF")
-    #     st.image(img_iyr_pan_tearsheet)
-    # with image_column_4:
-    #     st.write("VanEck Retail ETF")
-    #     st.image(img_rth_pan_tearsheet)
-    # with image_column_5:
-    #     st.write("Energy Select Sector SPDR ETF Fund")
-    #     st.image(img_xle_pan_tearsheet)
-    # with image_column_6:
-    #     st.write("Exxon Mobil Corp")
-    #     st.image(img_xom_pan_tearsheet)
-    # with image_column_7:
-    #     st.write("S&P500 ETF")
-    #     st.image(img_spy_pan_tearsheet)
+
+
